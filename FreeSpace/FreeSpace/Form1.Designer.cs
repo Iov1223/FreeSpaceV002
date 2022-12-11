4d680c3e1f2b1bbce57c505f158bf466834f5811
iov1223 <101730539+Iov1223@users.noreply.github.com>
2022-12-11 18:56:27 +0500

    Add files via upload
    1

---
+++ FreeSpace/FreeSpace/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FreeSpace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDiskPlace = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelDiskName = new System.Windows.Forms.Label();
+            this.comboBoxChoiseDisk = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelDiskPlace
+            // 
+            this.labelDiskPlace.AutoSize = true;
+            this.labelDiskPlace.Location = new System.Drawing.Point(13, 125);
+            this.labelDiskPlace.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDiskPlace.Name = "labelDiskPlace";
+            this.labelDiskPlace.Size = new System.Drawing.Size(0, 16);
+            this.labelDiskPlace.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Green;
+            this.progressBar1.Location = new System.Drawing.Point(16, 93);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(241, 28);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // labelDiskName
+            // 
+            this.labelDiskName.AutoSize = true;
+            this.labelDiskName.Location = new System.Drawing.Point(13, 73);
+            this.labelDiskName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDiskName.Name = "labelDiskName";
+            this.labelDiskName.Size = new System.Drawing.Size(0, 16);
+            this.labelDiskName.TabIndex = 2;
+            // 
+            // comboBoxChoiseDisk
+            // 
+            this.comboBoxChoiseDisk.FormattingEnabled = true;
+            this.comboBoxChoiseDisk.Location = new System.Drawing.Point(13, 24);
+            this.comboBoxChoiseDisk.Name = "comboBoxChoiseDisk";
+            this.comboBoxChoiseDisk.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxChoiseDisk.TabIndex = 3;
+            this.comboBoxChoiseDisk.Text = "Выбор диска";
+            this.comboBoxChoiseDisk.SelectedIndexChanged += new System.EventHandler(this.comboBoxChoiseDisk_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.comboBoxChoiseDisk);
+            this.Controls.Add(this.labelDiskName);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelDiskPlace);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDiskPlace;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelDiskName;
+        private System.Windows.Forms.ComboBox comboBoxChoiseDisk;
+    }
+}
+
---
+++ FreeSpace/FreeSpace/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace FreeSpace
+{
+    public partial class Form1 : Form
+    {
+        private DriveInfo[] allDrives = DriveInfo.GetDrives();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetSize()
+        {
+            var size = SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            var newSize = new Size(size.Width = 230, size.Height = 170);
+            this.MinimumSize = this.MaximumSize = this.Size = newSize;
+        }
+        private void AddToComboBox()
+        {
+            foreach (var drive in allDrives)
+            {
+                 comboBoxChoiseDisk.Items.Add(drive);
+            }
+        }
+        private float GetFreeDiskSpace(int index)
+        {
+            double GB = allDrives[index].AvailableFreeSpace / Math.Pow(1024, 3);
+            float _cb = (float)GB;
+            return _cb;
+        }
+        private float GetTotalDiskSpace(int index)
+        {
+            double GB = allDrives[index].TotalSize / Math.Pow(1024, 3);
+            float _cb = (float)GB;
+            return _cb;
+        }
+        private float GetMaximum(int index)
+        {
+            double GB = allDrives[index].TotalSize / Math.Pow(1024, 3);
+            float _cb = (float)GB;
+            return _cb;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddToComboBox();
+            GetSize();
+        }
+        public void ShowProgressBar()
+        {
+            progressBar1.ForeColor = Color.Green;
+            progressBar1.Value = 0;
+            int index = comboBoxChoiseDisk.SelectedIndex;
+            labelDiskName.Text = "Диск " + allDrives[index].Name;
+            progressBar1.Minimum = 0;
+            float _value = GetTotalDiskSpace(index) - GetFreeDiskSpace(index);
+            progressBar1.Maximum = (int)GetMaximum(index);
+            progressBar1.Value += (int)_value;
+            float _freePercent = GetFreeDiskSpace(index) / GetMaximum(index) * 100;
+            if (_freePercent > 75)
+            {
+                progressBar1.ForeColor = Color.Red;
+            }
+        }
+
+        private void comboBoxChoiseDisk_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBoxChoiseDisk.SelectedIndex;
+            ShowProgressBar();
+            labelDiskPlace.Text = GetFreeDiskSpace(index).ToString() + " ГБ свободно из "
+                + GetTotalDiskSpace(index).ToString();
+        }
+    }
+}
